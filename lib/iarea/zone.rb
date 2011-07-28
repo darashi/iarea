aa2250a3c9fd76ee157a347662679b188afe6e06
SHIDARA Yoji <dara@shidara.net>
2011-07-28 13:14:58 +0900

    add docs for Iarea::Zone

--- lib/iarea/zone.rb
+++ lib/iarea/zone.rb
@@ -2,12 +2,14 @@ module Iarea
   class Zone < OpenStruct
     @@zones = []
 
+    # Prefectures in the zone
     def prefectures
       DB[:prefectures].where(:zone_id => self.id).select(:id).order(:id).map do |prefecture|
         Prefecture.find(prefecture[:id])
       end
     end
 
+    # Areas in the zone
     def areas
       DB[:areas].where(:zone_id => self.id).select(:areacode).order(:areacode).map do |area|
         Area.find(area[:areacode])
@@ -15,10 +17,12 @@ module Iarea
     end
 
     class << self
+      # Find a zone by <tt>id</tt>
       def find(id)
         @@zones[id] ||= new DB[:zones].where(:id => id).first
       end
 
+      # All zones
       def all
         DB[:zones].select(:id).order(:id).all.map{ |z| find(z[:id]) }
       end
